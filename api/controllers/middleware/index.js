7e0bc68242315227ceaa04e140d1bc3f6aa00efa
Josh Levy <Josh Levy>
2016-10-23 10:10:21 -0600

    Transferred new files into project

--- api/controllers/middleware/index.js
+++
@@ -1,6 +0,0 @@
-var middleware = {};
-
-// adds cross domain functionality, in a very loose way
-middleware.allowAllCors = require('./lib/allowAllCors');
-
-module.exports = middleware;
--- config/production.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apiServer: '/api/',
-  parse: {
-    appKey: 'gp5pTelFu2HAZb4JQcrNVhLVAjmTwn7ysUF8ySSe',
-    jsKey:  'sHWp3LeeMD4FARjm6eVpAsft8lO7Wxa4lNM8Vaw6'
-  }
-}
